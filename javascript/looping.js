adb4dca8c0061fc468f4a4437fd8f15cd773bdd7
chandan <chandansamantaray784@gmail.com>
2024-06-20 22:01:07 +0530

    loop

---
+++ javascript/looping.js
@@ -0,0 +1,41 @@
+//table program
+
+// let num = 4;
+
+// console.log(2 * 1);
+// console.log(2 * 2);
+// console.log(2 * 3);
+
+// for (let i = 1; i < 11; i++) {
+//   console.log(num * i);
+// }
+
+// let i = 1;
+// while (i < 11) {
+//   console.log(num * i);
+
+//   i++;
+// }
+// let i = 1;
+// do {
+//   console.log(num * i);
+//   i++;
+// } while (i < 11);
+
+//decrement order
+
+// let y = 10;
+
+// for (let i = y; i > 0; i--) {
+//   console.log(i);
+// }
+
+//find even number between given number
+
+// let number = 20;
+
+// for (let i = 1; i < number; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
